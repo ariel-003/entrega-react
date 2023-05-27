0bb82305e68c739e55a766274315a5a022b3fad6
cristian <arielcou@gmail.com>
2023-05-27 17:33:14 -0300

    entrega

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,22 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card } from "./Card";
 
-function Product(props) {
-  return (
-    <Link style={{textDecoration: 'none'}} to={`/item/${props.product.id}`}>
+function Product(products) {
+  return ( products.map(product => 
+    <Link style={{textDecoration: 'none'}} to={`/item/${product.id}`}>
       <Card className="card shadow-lg" style={{ width: "18rem" }}>
         <img
-          src={props.product.image_principal}
-          alt={props.product.id}
+          src={product.image_principal}
+          alt={product.id}
           className="card-img-top"
         />
         <div className="card-body text-center">
-          <h5 className="title card-title">{props.product.title}</h5>
-          <p className="card-text">{props.product.description}</p>
-          <p className="price card-text"> $ {props.product.price}</p>
+          <h5 className="title card-title">{product.title}</h5>
+          <p className="card-text">{product.description}</p>
+          <p className="price card-text"> $ {product.price}</p>
         </div>
       </Card>
     </Link>
+  )
   );
 }
 
